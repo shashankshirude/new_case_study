d45457b7848c9d0f6fde89bf0cec3d2eae6f15f8
Shashank <shashankshirude@gmail.com>
2023-12-19 11:37:11 +0530

    -

--- template.py
+++ template.py
@@ -20,7 +20,8 @@ list_of_files = [
     "params.yaml",
     "requirments.txt",
     "setup.py",
-    "research/trials.ipynb"
+    "research/trials.ipynb",
+    "templates/index.html"
 ]
 
 for file_path in list_of_files:
